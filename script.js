0d549762245c7534aa68c62cc8b7446c2b99794c
misterKumar <120304503+misterKumar@users.noreply.github.com>
2023-05-13 15:21:04 +0530

    sol

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map((e)=>{
+    if(e.marks>50) console.log(e);
+});
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((element)=>{
+    if(element.marks>50){
+        console.log(element);
+    }
+});
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({id:4,name:"Mister",age:"22",marks: 90})
+    console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr= arr.filter((e)=>{
+    return e.marks>50
+})
+console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let array_to_concatenate=[
+    {id:4,name:"ravi",age:"20",marks:45},
+    {id:5,name:"ram",age:"22",marks:60},
+    {id:6,name:"das",age:"19",marks:75}
+];
+
+arr=arr.concat(array_to_concatenate)
+console.log(arr);
 }
